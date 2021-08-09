3f011f32d8005a658f76e174a7bb2e813c53ec89
0217tatsunoko <88361117+yiscode@users.noreply.github.com>
2021-08-09 13:08:58 +0000

    export docx

--- fetchannel.py
+++ fetchannel.py
@@ -1,4 +1,7 @@
 import discord
+from docx import Document
+from docx.shared import Inches
+import os
 async def fetchannel(message):
   messages = await message.channel.history(limit=None).flatten()
   context = [str(i.content) for i in messages]
@@ -7,7 +10,26 @@ async def fetchannel(message):
 def convertstring(list):
   
   list.reverse()
-  changelist=[str(i).replace("'","").replace("'","").replace("(","").replace(")","").lstrip().replace(",","").replace("!export","").replace("List show all","").replace("successfully add","").replace("List add","") for i in list]
+  changelist=[str(i).replace("'","").replace("'","").replace("(","").replace(")","").lstrip().replace(",","").replace("!export","").replace("List show all","").replace("successfully add","").replace("List add","").lstrip() for i in list]
   
   listostring = "\n".join(changelist)
   return listostring
+async def convertdocx(message):
+  messages = await message.channel.history(limit=None).flatten()
+  messages.reverse()
+  document = Document()
+  for i in messages:
+    if len(i.content)>0:
+      inside = str(i.content).replace("'","").replace("'","").replace("(","").replace(")","").lstrip().replace(",","").replace("!export","").replace("List show all","").replace("successfully add","").replace("List add","").lstrip()
+      document.add_paragraph(inside)
+    if len(i.attachments) > 0:
+        attachment = i.attachments[0]
+        if attachment.filename.endswith(".jpg") or attachment.filename.endswith(".jpeg") or attachment.filename.endswith(".png") or attachment.filename.endswith(".webp") or attachment.filename.endswith(".gif"):
+            await attachment.save('1.png')
+            document.add_picture('1.png')
+            os.remove('1.png')
+
+  document.add_page_break()
+  document.save('demo.docx')
+
+
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from keepwake import keep_alive
 import discord
 import os
 from fetchannel import fetchannel
+from fetchannel import convertdocx as cod
+
 client = discord.Client()
 @client.event
 async def on_ready():
@@ -14,10 +16,12 @@ async def on_message(message):
   if '!export' in message.content:
     
     await fetchannel(message)
-    
+    await cod(message)
     
     await message.channel.send(file=discord.File('file.txt'))
+    await message.channel.send(file=discord.File('demo.docx'))
     os.remove('file.txt')
+    os.remove('demo.docx')
   
 keep_alive()
 client.run(os.getenv('TOKEN'))
